74251082855e201c4e98715fc30700aa747cd045
pnegron <pnegron@pauls-mbp-2.home>
2018-03-07 19:25:37 -0700

    lab file change-01

--- python-labs/DA-1.py
+++ python-labs/DA-1.py
@@ -1,14 +1,16 @@
-#import the neccessary modules
+# import the neccessary modules
+import requests
+import getpass
+import simplejson as json
 
 
+# setup the variables used in the script
 
-#setup the variables used in the script 
- 
 ip = '192.168.10.1'
 username = "admin"
 password = getpass.getpass()
 
-#Execute the main part of the code
+# Execute the main part of the code
 
 login_url = 'https://{}/api/aaaLogin.json'.format(ip)
 name_pwd = {'aaaUser': {'attributes': {'name': username, 'pwd': password}}}
@@ -16,7 +18,7 @@ json_credentials = json.dumps(name_pwd)
 
 # log in to API - Disable login warnings
 
-requests.packages.urllib3.disable_warnings()  
+requests.packages.urllib3.disable_warnings()
 post_response = requests.post(login_url, data=json_credentials, verify=False)
 
 # extract token from login response structure
@@ -25,4 +27,4 @@ login_attributes = auth['imdata'][0]['aaaLogin']['attributes']
 auth_token = login_attributes['token']
 cookies = {}
 cookies['APIC-cookie'] = auth_token
-return cookies
+print(cookies)
--- python-labs/LAB-6.py
+++ python-labs/LAB-6.py
@@ -1,31 +1,38 @@
-#import the neccessary modules
+# import the neccessary modules
 
-import time
-import sys
+import requests
 import getpass
-import paramiko
+import simplejson as json
 
-#setup the variables used in the script 
- 
-ip = '10.10.10.1'
-username = "admin"
-password = getpass.getpass()
 
-#Execute the main part of the code
+# setup the variables used in the script
 
-login_url = 'https://{}/api/aaaLogin.json'.format(ip)
-name_pwd = {'aaaUser': {'attributes': {'name': username, 'pwd': password}}}
-json_credentials = json.dumps(name_pwd)
 
-# log in to API - Disable login warnings
+def Login_ACI():
+    ip = '192.168.10.1'
+    username = "admin"
+    password = getpass.getpass()
 
-requests.packages.urllib3.disable_warnings()  
-post_response = requests.post(login_url, data=json_credentials, verify=False)
+    # Execute the main part of the code
 
-# extract token from login response structure
-auth = json.loads(post_response.text)
-login_attributes = auth['imdata'][0]['aaaLogin']['attributes']
-auth_token = login_attributes['token']
-cookies = {}
-cookies['APIC-cookie'] = auth_token
-return cookies
+    login_url = 'https://{}/api/aaaLogin.json'.format(ip)
+    name_pwd = {'aaaUser': {'attributes': {'name': username,
+                                           'pwd': password}}}
+    json_credentials = json.dumps(name_pwd)
+
+    # log in to API - Disable login warnings
+
+    requests.packages.urllib3.disable_warnings()
+    post_response = requests.post(login_url, data=json_credentials,
+                                  verify=False)
+
+    # extract token from login response structure
+    auth = json.loads(post_response.text)
+    login_attributes = auth['imdata'][0]['aaaLogin']['attributes']
+    auth_token = login_attributes['token']
+    cookies = {}
+    cookies['APIC-cookie'] = auth_token
+    print(cookies)
+
+x = Login_ACI()
+print(x)
